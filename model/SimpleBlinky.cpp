00bf166851705ea5807ed4b13cd8163167f4b17d
aoshiga <titouan.buronfosse@gmail.com>
2021-12-07 15:38:21 +0100

    Create branch: try to create a shape

--- model/SimpleBlinky.cpp
+++ model/SimpleBlinky.cpp
@@ -3,6 +3,9 @@
 #include<map>
 #include<list>
 #include<set>
+#include <stdlib.h>
+#include <stdio.h>
+
 using namespace SimpleBlinky;
 
 namespace reconfigure {
@@ -55,64 +58,116 @@ namespace reconfigure {
 
     //BlinkyBlock Map 
     class BBMap {
-        public:
-        list<Node*> blocks;
+        private:
+        typedef struct surface {
+            struct surface *left;
+            struct surface *right;
+            struct surface *top;
+            struct surface *bottom;
+            struct surface *front;
+            struct surface *back;
+        } surfaces;
 
-        Node * extend() {
+        surfaces * initSurfaces() {
+            surfaces *l = new surfaces;
+            l->left = NULL;
+            l->right = NULL;
+            l->top = NULL;
+            l->bottom = NULL;
+            l->front = NULL;
+            l->back = NULL;
+            return l;
+        }
+
+        public:
+        map<Node*, surfaces*> blocks;
+
+        Node * initialize() {
             Node* newNode = new Node();
-            blocks.push_back(newNode);
+            blocks.insert(pair<Node*, surfaces*>(newNode, initSurfaces()));
             return newNode;
         }
 
-        void erase(Node * node) {
-            auto it = blocks.begin();
-            for(; it != blocks.end(); ++ it) {
-                if((*it) == node)
-                    break;
-        }
-            if(it == blocks.end())
+        void insertLeft(Node *from, Node *newNode) {
+            surfaces *s = blocks.find(from)->second;
+            if(s->left) {
+                printf("Left already connected");
                 return;
-
-            blocks.erase(it);
+            }
+            s->left = (surfaces*) malloc(sizeof(surfaces));
+            (s->left)->left = NULL;
+            (s->left)->right = s;
+            (s->left)->top = NULL;
+            (s->left)->bottom = NULL;
+            (s->left)->front = NULL;
+            (s->left)->back = NULL;
+            
+            blocks.insert(pair<Node*, surfaces*>(newNode, s->left));
         }
 
-        //Direction: 1 left (clockwise), 2 right (counter-clockwise)
+        //Direction: 1 left, 2 right, 3 top, 4 bottom, 5 front, 6 back
         bool isNeighbour(Node *n1, Node *n2, int direction) {
-            auto it = blocks.begin();
-            for(; it != blocks.end(); ++ it)
-                if((*it) == n1)
+            if(n1 && n2) {
+                switch (direction) {
+                case 1:
+                    (blocks.find(n1)->second->left == blocks.find(n2)->second ) ? true : false; 
+                    break;
+                
+                case 2:
+                    (blocks.find(n1)->second->right == blocks.find(n2)->second ) ? true : false; 
                     break;
 
-            if(it == blocks.end())
-                return false;
+                case 3:
+                (blocks.find(n1)->second->top == blocks.find(n2)->second ) ? true : false; 
+                    break;
 
-            if(direction == 1) {
-                it++;
-                if(it == blocks.end()) {
-                    return *(blocks.begin()) == n2;
-                }
-                    else {
-                    return *(it) == n2;
+                case 4:
+                    (blocks.find(n1)->second->bottom == blocks.find(n2)->second ) ? true : false; 
+                    break;
+                
+                case 5:
+                    (blocks.find(n1)->second->front == blocks.find(n2)->second ) ? true : false; 
+                    break;
+                
+                case 6:
+                    (blocks.find(n1)->second->back == blocks.find(n2)->second ) ? true : false; 
+                    break;
+
+                default:
+                    return false;
+                    break;
                 }
             }
-            else if(direction == 2) {
-                if(it == blocks.begin())
-                    return *(blocks.end()) == n2;
-                else {
-                    it--;
-                    return *(it) == n2;
-                }
-            }
-
             return false;
         }
 
-        void print() {
-            cout << this << ":";
-            for(auto elem = blocks.begin(); elem != blocks.end(); ++elem) {
-                cout << " " << *elem;
+        //Direction: 1 left, 2 right, 3 top, 4 bottom, 5 front, 6 back
+        bool hasNeighbour(Node *n, int direction) {
+            if(!n) return false;
+
+            switch (direction) {
+            case 1:
+                (blocks.find(n)->second->left != NULL) ? true : false; 
+                break;
+            case 2:
+                (blocks.find(n)->second->right != NULL) ? true : false; 
+                break;
+            case 3:
+                (blocks.find(n)->second->top != NULL) ? true : false; 
+                break;
+            case 4:
+                (blocks.find(n)->second->bottom != NULL) ? true : false; 
+                break;
+            case 5:
+                (blocks.find(n)->second->front != NULL) ? true : false; 
+                break;
+            case 6:
+                (blocks.find(n)->second->back != NULL) ? true : false; 
+                break;
+            default:
+                return false;
+                break;
             }
-            cout << endl;
         }
     };
 }
